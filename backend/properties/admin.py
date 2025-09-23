709584a559d0cf2938ba2665df969e29e249fea9
Floyd-Pinto <floydpinto5@gmail.com>
2025-09-23 13:23:28 +0530

    Restarted Backend File, Updated the Gitignore and the python venv

--- backend/properties/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Property
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'property_type', 'price', 'location', 'owner', 'created_at')
-    list_filter = ('property_type', 'location', 'created_at')
-    search_fields = ('title', 'description', 'location')
-    readonly_fields = ('created_at', 'updated_at')
